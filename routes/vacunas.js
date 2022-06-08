54aa8ef054fb47b5501589db7f602d23050a203f
Rodo-Caspya <87332892+Rodo-Caspya@users.noreply.github.com>
2022-06-08 17:06:32 -0500

    UPDATE

--- routes/vacunas.js
+++ routes/vacunas.js
@@ -29,6 +29,25 @@ router.get('/vacunas', async (req,res) => {
         res.status(200).json("Error");
     }
 });
-
-
+router.put('/update/:id', async function update (req, res){
+    try{
+        const vacuna = await vac.findById(req.params.id);
+        Object.assign(vacuna,req.body);
+        vacuna.save();
+        res.status(200).json({success: true, status: 'Successful!'});
+    }catch{
+        res.status(200).send({Error: "Vacuna is not found"});
+    }
+    
+  });
+router.delete('/delete/:id', async function deleteById (req, res){
+    console.log("hi");
+    try{
+        const vacuna = await vac.findById(req.params.id);
+        await vacuna.remove();
+        res.status(200).json({success: true, status: 'Successful!'});
+    }catch{ 
+        res.status(200).send({Error: "Vacuna is not found"});
+    }
+  });
 module.exports = router;
